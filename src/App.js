76217d2729f2e334ae21578c694029a792332526
Dubei Anatolii <105717456+DubeiA@users.noreply.github.com>
2023-04-21 11:06:26 +0300

    update app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 // import logo from './logo.svg';
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [inc, setInc] = useState(0);
+
+  const counter = () => {
+    setInc((prevInc) => prevInc + 1);
+  };
   return (
     <div className="App">
       <p>Hi there</p>
+      <h1> I create a new project</h1>
+
+      <button onClick={counter}> + 1 {inc}</button>
     </div>
   );
 }
