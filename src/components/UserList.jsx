6d4b608978eb326053a1fe038ed2564a865cdc81
Dubei Anatolii <105717456+DubeiA@users.noreply.github.com>
2023-04-22 14:46:08 +0300

    add request for change page

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -1,31 +1,53 @@
 import { useState, useEffect } from 'react';
-import { fetchUsers } from './api/usersAPI';
+import { fetchUsers, nextPageUsers } from './api/usersAPI';
 import css from './UserList.module.css';
 
 export const UserList = () => {
   const [users, setUsers] = useState([]);
   const [userId, setUserId] = useState(null);
-  const [pressTwice, setPressTwice] = useState(false);
+  const [page, setPage] = useState(2);
+  // const [pressTwice, setPressTwice] = useState(true);
+  // const [isFollow, setIsFollow] = useState(false);
   // const [followerCount, setFollowerCount] = useState(null);
 
-  console.log(pressTwice);
+  // console.log(pressTwice);
 
   const getUsersFromAPI = async () => {
     const user = await fetchUsers();
     setUsers(user);
   };
+
+  console.log(users);
+
+  const fetchNextPage = async firth => {
+    setPage(prevPage => prevPage + 1);
+    const usersNext = await nextPageUsers(page, 3);
+    setUsers(usersNext);
+    if (page === 4) {
+      setPage(1);
+      await nextPageUsers(page, 3);
+      return;
+    }
+
+    return usersNext;
+  };
+
   useEffect(() => {
     getUsersFromAPI();
   }, []);
 
   const handleFollowClick = id => {
+    const userIdx = users.findIndex(user => user.id === id);
+    console.log(userIdx);
     if (userId === id) {
       setUserId(null);
-      setPressTwice(true);
-    } else {
-      setUserId(id);
-      setPressTwice(false);
+      users[userIdx].followers--;
+
+      return;
     }
+    setUserId(id);
+    users[userIdx].followers++;
+    // setPressTwice(false);
   };
 
   return (
@@ -69,6 +91,11 @@ export const UserList = () => {
             );
           })}
         </ul>
+      )}
+      {page <= 4 && (
+        <button type="button" onClick={() => fetchNextPage()}>
+          Next
+        </button>
       )}
     </div>
   );
