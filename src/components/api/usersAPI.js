27b86874787b3bd83eaff0a8d9661253c905dce9
Dubei Anatolii <105717456+DubeiA@users.noreply.github.com>
2023-04-24 09:26:38 +0300

    update api

--- src/components/api/usersAPI.js
+++ src/components/api/usersAPI.js
@@ -3,19 +3,26 @@ import axios from 'axios';
 axios.defaults.baseURL = 'https://64424fbf33997d3ef90cf4f2.mockapi.io/';
 
 export async function fetchUsers() {
-  const response = await axios.get('users', {
-    params: { page: 1, limit: 3 },
-  });
+  try {
+    const response = await axios.get('users', {
+      params: { page: 1, limit: 3 },
+    });
 
-  console.log('response.data', response.data);
-  return response.data;
+    return response.data;
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 export async function nextPageUsers(page, limit) {
-  const response = await axios.get('users', {
-    params: { page, limit },
-  });
-  return response.data;
+  try {
+    const response = await axios.get('users', {
+      params: { page, limit },
+    });
+    return response.data;
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 // export async function userFollowers(id) {
